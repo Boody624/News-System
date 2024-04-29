a9d45159028448f51d8d850b0213146697f8cdcf
omarramy22 <159173551+omarramy22@users.noreply.github.com>
2024-04-29 18:54:50 +0300

    Add files via upload

--- user.cpp
+++ user.cpp
@@ -12,3 +12,34 @@ user::~user()
 {
     delete ui;
 }
+
+void user::on_pushButton_clicked()
+{
+    if (ui->title->isChecked())
+    {
+        // search code with the titles of other news
+    }
+    else if (ui->date->isChecked())
+    {
+      // search code with the dates of other news
+    }
+    else if (ui->keyword->isChecked())
+    {
+        // search code with the keyword of other news
+    }
+}
+
+
+void user::on_pushButton_2_clicked()
+{
+    // does something to the rating
+    ui->rate->currentIndex();
+}
+
+
+void user::on_pushButton_3_clicked()
+{
+    //does something to the news
+    ui->filter->currentText();
+}
+
--- user.h
+++ user.h
@@ -15,6 +15,13 @@ public:
     explicit user(QWidget *parent = nullptr);
     ~user();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
 private:
     Ui::user *ui;
 };
