09ec74969320da2fa3d0f1ce4569ae8d9803d381
Abdelrahman <148652117+Boody624@users.noreply.github.com>
2024-05-02 14:22:12 +0300

    Add files via upload

--- adminview.cpp
+++ adminview.cpp
@@ -11,11 +11,43 @@ adminView::adminView(QWidget *parent)
     ui->setupUi(this);
     ui->verticalLayout = new QVBoxLayout(ui->scrollAreaWidgetContents);
     ui->scrollArea->setWidget(ui->scrollAreaWidgetContents);
+    posts.setFileName("posts.txt");
+    loadPosts();
 }
 
 adminView::~adminView()
 {
     delete ui;
+}
+
+void adminView::loadPosts()
+{
+    isAdmin = true;
+    QFile inFile("/Users/kareem/Desktop/postsFile.txt");
+
+    if (!inFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "Failed to open the file for reading.";
+    }
+
+    QTextStream inStream(&inFile);
+
+    while(!inStream.atEnd())
+    {
+        for(int i=0; i<5; i++)
+        {
+            Post *newP = new Post;
+            newP->title = inStream.readLine();
+            newP->description = inStream.readLine();
+            newP->date = QDate::fromString(inStream.readLine(), "ddd MMM dd yyyy");
+            newP->rating = inStream.readLine().toInt();
+            newP->category = inStream.readLine();
+            if(newP->rating!=0)
+                addPost(newP);
+        }
+    }
+
+
+
 }
 
 void adminView::on_pushButton_clicked()
@@ -27,6 +59,7 @@ void adminView::on_pushButton_clicked()
 }
 
 void adminView::addPost(Post *p) {
+    PostsVector.push_back(p);
     QWidget *postWidget = new QWidget(ui->scrollAreaWidgetContents);
     QVBoxLayout *postLayout = new QVBoxLayout(postWidget);
 
@@ -35,6 +68,11 @@ void adminView::addPost(Post *p) {
     QLabel *date = new QLabel(p->date.toString());
     QLabel *rating = new QLabel("Rating: "+QString::number(p->rating)+ " Stars");
     QLabel *category = new QLabel(p->category);
+
+
+
+
+
 
     description->setWordWrap(true);
     postLayout->addWidget(title);
@@ -53,6 +91,13 @@ void adminView::addPost(Post *p) {
         connect(deleteButton, &QPushButton::clicked, [=]() {
             ui->verticalLayout->removeWidget(postWidget);
             delete postWidget;
+            for(int i=0; i<PostsVector.size(); i++)
+                {
+                if(PostsVector[i] == p)
+                {
+                    PostsVector.erase(PostsVector.begin()+i);
+                }
+            }
             delete p;
         });
 
@@ -85,3 +130,28 @@ void adminView::addPost(Post *p) {
 
     ui->verticalLayout->addWidget(postWidget);
 }
+
+void adminView::on_pushButton_2_clicked()
+{
+    QFile postsFile("/Users/kareem/Desktop/postsFile.txt");
+    if (!postsFile.open(QIODevice::WriteOnly| QIODevice::Text)) {
+        qDebug() << "Failed to open file for writing: " << postsFile.errorString();
+        return;
+    }
+    QTextStream outFile(&postsFile);
+
+    for(int i=0; i<PostsVector.size(); i++)
+    {
+        outFile << PostsVector[i]->title << Qt::endl;
+        outFile << PostsVector[i]->description << Qt::endl;
+        outFile << PostsVector[i]->date.toString() <<Qt::endl;
+        outFile << PostsVector[i]->rating << Qt::endl;
+        outFile << PostsVector[i]->category <<Qt::endl;
+
+    }
+    postsFile.flush();
+    postsFile.close();
+    exit(0);
+
+}
+
